1de498cdc14ce11eb6f68089a7132649d20e046d
mcpecommander <hosam201520152015@gmail.com>
2018-05-20 15:27:16 +0200

    More checks to make the mob fair.

--- src/main/java/mcpecommander/theOvercasted/entity/entities/EntityManEater.java
+++ src/main/java/mcpecommander/theOvercasted/entity/entities/EntityManEater.java
@@ -14,10 +14,10 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.EnumHand;
 import net.minecraft.world.World;
 
-public class EntityManEater extends EntityMob implements IAnimated{
-	
+public class EntityManEater extends EntityMob implements IAnimated {
+
 	private static AnimationHandler animHandler = CraftStudioApi.getNewAnimationHandler(EntityManEater.class);
-	
+
 	static {
 		EntityManEater.animHandler.addAnim(Reference.MODID, "man_eat", "man_eater", false);
 	}
@@ -26,7 +26,7 @@ public class EntityManEater extends EntityMob implements IAnimated{
 		super(worldIn);
 		this.setSize(1.2f, 0.2f);
 	}
-	
+
 	@Override
 	public void applyEntityAttributes() {
 		super.applyEntityAttributes();
@@ -34,63 +34,68 @@ public class EntityManEater extends EntityMob implements IAnimated{
 		this.getEntityAttribute(SharedMonsterAttributes.KNOCKBACK_RESISTANCE).setBaseValue(1.0D);
 		this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(3.0D);
 	}
-	
+
 	@Override
 	protected void onDeathUpdate() {
 		super.onDeathUpdate();
-		if(this.isBeingRidden()) {
-			this.getPassengers().forEach(entity ->entity.dismountRidingEntity());
+		if (this.isBeingRidden()) {
+			this.getPassengers().forEach(entity -> entity.dismountRidingEntity());
 		}
 	}
-	
+
 	@Override
 	public void onLivingUpdate() {
 		super.onLivingUpdate();
 		this.rotationYawHead = 0;
 		this.getAnimationHandler().animationsUpdate(this);
-		if(this.isWorldRemote() ) {
-			if(this.isBeingRidden()) {
+		if (this.isWorldRemote()) {
+			if (this.isBeingRidden()) {
 				AnimationHelper.startHoldAnimation(animHandler, this, "man_eat");
-			}else {
+			} else {
 				AnimationHelper.stopAnimation(animHandler, this, "man_eat");
 			}
-		}else {
+		} else {
 
 		}
-		if(this.isBeingRidden() && this.getPassengers().get(0) instanceof EntityPlayer) {
+		if (this.isBeingRidden() && this.getPassengers().get(0) instanceof EntityPlayer) {
 			EntityPlayer player = (EntityPlayer) this.getPassengers().get(0);
-			if(this.ticksExisted%3 == 0) this.attackEntityAsMob(player);
-			//The cheapest way to stop a player from dismounting this.
-			if(player.isSneaking()) {
+			if (this.ticksExisted % 3 == 0 && !player.capabilities.isCreativeMode)
+				this.attackEntityAsMob(player);
+			// The cheapest way to stop a player from dismounting this.
+			if (player.isSneaking()) {
 				player.setSneaking(false);
 			}
-		}else {
+		} else {
 			this.setSize(1.2f, 0.2f);
 		}
 	}
-	
-	//We increase the size to make it easier to attack the mob when being trapped so we adjust the mounting height too.
+
+	// We increase the size to make it easier to attack the mob when being trapped
+	// so we adjust the mounting height too.
 	@Override
 	public double getMountedYOffset() {
 		return this.height * .2f;
 	}
-	
-	//The player is trapped not riding so we do not render it in riding position.
+
+	// The player is trapped not riding so we do not render it in riding position.
 	@Override
 	public boolean shouldRiderSit() {
 		return false;
 	}
-	
-	//Should not be pushed.
+
+	// Should not be pushed.
 	@Override
 	public boolean canBePushed() {
 		return false;
 	}
-	
-	//The fucking canCollide boolean does not stop collision so we override this and add our trapping code.
+
+	// The fucking canCollide boolean does not stop collision so we override this
+	// and add our trapping code.
 	@Override
 	protected void collideWithEntity(Entity entityIn) {
-		if(entityIn instanceof EntityPlayer && !this.isBeingRidden() && !((EntityPlayer) entityIn).capabilities.isCreativeMode && this.deathTime < 1) {
+		if (entityIn instanceof EntityPlayer && !this.isBeingRidden()
+				&& !((EntityPlayer) entityIn).capabilities.isCreativeMode && this.deathTime < 1 && !entityIn.isRiding()
+				&& !entityIn.isBeingRidden() && !((EntityPlayer) entityIn).isSpectator()) {
 			entityIn.startRiding(this, true);
 			this.playSound(ModSounds.man_eater_trap, 2f, 1.5f);
 			this.setSize(2.3f, 2.3f);
