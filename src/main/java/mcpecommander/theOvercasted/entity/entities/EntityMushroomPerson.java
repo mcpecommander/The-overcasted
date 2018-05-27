8cc21bb78bfa04ef380666955b20ea457dfba62a
mcpecommander <hosam201520152015@gmail.com>
2018-05-27 19:16:23 +0200

    animation testing

--- src/main/java/mcpecommander/theOvercasted/entity/entities/EntityMushroomPerson.java
+++ src/main/java/mcpecommander/theOvercasted/entity/entities/EntityMushroomPerson.java
@@ -4,16 +4,30 @@ import com.leviathanstudio.craftstudio.CraftStudioApi;
 import com.leviathanstudio.craftstudio.common.animation.AnimationHandler;
 import com.leviathanstudio.craftstudio.common.animation.IAnimated;
 
+import mcpecommander.theOvercasted.Reference;
+import mcpecommander.theOvercasted.util.AnimationHelper;
+import net.minecraft.entity.IEntityLivingData;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.World;
 
 public class EntityMushroomPerson extends EntityBaseAnimated {
 
 	// The animation handler from the api.
 	private static AnimationHandler animHandler = CraftStudioApi.getNewAnimationHandler(EntityMushroomPerson.class);
+	public int size;
+	
+	static {
+		EntityMushroomPerson.animHandler.addAnim(Reference.MODID, "mushroom_walk", "mushroom_person", true);
+		EntityMushroomPerson.animHandler.addAnim(Reference.MODID, "mushroom_hide", "mushroom_person", false);
+		EntityMushroomPerson.animHandler.addAnim(Reference.MODID, "mushroom_punch", "mushroom_person", false);
+	}
 
 	public EntityMushroomPerson(World worldIn) {
 		super(worldIn);
-		this.setSize(2f, 2f);
+		size = this.getRNG().nextInt(2);
+		this.setSize(.8f + size/2.8f, 1.1f + size);
+		
 	}
 
 	@Override
@@ -22,9 +36,37 @@ public class EntityMushroomPerson extends EntityBaseAnimated {
 	}
 	
 	@Override
+	public IEntityLivingData onInitialSpawn(DifficultyInstance difficulty, IEntityLivingData livingdata) {
+		return super.onInitialSpawn(difficulty, livingdata);
+	}
+	
+	@Override
+	public void writeEntityToNBT(NBTTagCompound compound) {
+		super.writeEntityToNBT(compound);
+		compound.setInteger("size", size);
+	}
+	
+	@Override
+	public void readEntityFromNBT(NBTTagCompound compound) {
+		super.readEntityFromNBT(compound);
+		if(compound.hasKey("size", 3)) {
+			size = compound.getInteger("size");
+		}
+	}
+	
+	@Override
 	public void onLivingUpdate() {
 		super.onLivingUpdate();
 		this.getAnimationHandler().animationsUpdate(this);
+		if(isWorldRemote()) {
+			if(this.size == 0) {
+				AnimationHelper.startHoldAnimation(animHandler, this, "mushroom_hide");
+			}else {
+				AnimationHelper.startAnimation(animHandler, this, "mushroom_punch");
+			}
+		}else {
+			
+		}
 	}
 
 }
--- src/main/java/mcpecommander/theOvercasted/entity/renderer/RenderMushroomPerson.java
+++ src/main/java/mcpecommander/theOvercasted/entity/renderer/RenderMushroomPerson.java
@@ -3,9 +3,11 @@ package mcpecommander.theOvercasted.entity.renderer;
 import mcpecommander.theOvercasted.Reference;
 import mcpecommander.theOvercasted.entity.CraftStudioModelSon;
 import mcpecommander.theOvercasted.entity.entities.EntityMushroomPerson;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.entity.Entity;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.client.registry.IRenderFactory;
 
@@ -14,12 +16,23 @@ public class RenderMushroomPerson extends RenderLiving<EntityMushroomPerson>{
 	private static CraftStudioModelSon model = new CraftStudioModelSon(Reference.MODID, "mushroom_person", 64, 32);
 	
 	public RenderMushroomPerson(RenderManager rendermanagerIn) {
-		super(rendermanagerIn, model, 0.7f);
+		super(rendermanagerIn, model, 0.3f);
 	}
-
+	
 	@Override
 	protected ResourceLocation getEntityTexture(EntityMushroomPerson entity) {
 		return new ResourceLocation(Reference.MODID, "textures/entity/mushroom_person.png");
+	}
+	
+	@Override
+	protected void renderModel(EntityMushroomPerson entitylivingbaseIn, float limbSwing, float limbSwingAmount,
+			float ageInTicks, float netHeadYaw, float headPitch, float scaleFactor) {
+		this.shadowSize = 0.3f + entitylivingbaseIn.size/3f;
+		GlStateManager.pushMatrix();
+		GlStateManager.scale(1d + entitylivingbaseIn.size, 1d + entitylivingbaseIn.size, 1d + entitylivingbaseIn.size);
+		GlStateManager.translate(0d, 0d - (entitylivingbaseIn.size/1.33d), 0d);
+		super.renderModel(entitylivingbaseIn, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scaleFactor);
+		GlStateManager.popMatrix();
 	}
 	
 	public static class Factory<T extends EntityMushroomPerson> implements IRenderFactory<T>{
